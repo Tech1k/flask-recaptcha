6c82d1546b50f5ed0fb13db273d089ef4fa9aae1
Kristian Kramer <kristian@kk.dev>
2023-01-05 23:26:48 -0700

    Fix code and bump version to 0.1.1

--- flask_turnstile.py
+++ flask_turnstile.py
@@ -3,7 +3,7 @@ A Cloudflare Turnstile extension for Flask based on flask-recaptcha
 """
 
 __NAME__ = "Flask-Turnstile"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "MIT"
 __author__ = "Kristian (originally ReCaptcha by Mardix)"
 __copyright__ = "(c) 2023 Kristian (originally ReCaptcha by Mardix 2015)"
@@ -18,6 +18,9 @@ try:
 except ImportError as ex:
     print("Missing dependencies")
 
+class BlueprintCompatibility(object):
+    site_key = None
+    secret_key = None
 
 class DEFAULTS(object):
     IS_ENABLED = True
@@ -32,8 +35,8 @@ class Turnstile(object):
 
     def __init__(self, app=None, site_key=None, secret_key=None, is_enabled=True, **kwargs):
         if site_key:
-            self.site_key = site_key
-            self.secret_key = secret_key
+            BlueprintCompatibility.site_key = site_key
+            BlueprintCompatibility.secret_key = secret_key
             self.is_enabled = is_enabled
 
         elif app:
@@ -54,18 +57,18 @@ class Turnstile(object):
         :return:
         """
         return "" if not self.is_enabled else ("""
-        <script src='//challenges.cloudflare.com/turnstile/v0/api.js'></script>
+        <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
         <div class="cf-turnstile" data-sitekey="{SITE_KEY}"></div>
-        """.format(SITE_KEY=self.site_key))
+        """.format(SITE_KEY=BlueprintCompatibility.site_key))
 
     def verify(self, response=None, remote_ip=None):
         if self.is_enabled:
             data = {
-                "secret": self.secret_key,
+                "secret": BlueprintCompatibility.secret_key,
                 "response": response or request.form.get('cf-turnstile-response'),
                 "remoteip": remote_ip or request.environ.get('REMOTE_ADDR')
             }
 
-            r = requests.get(self.VERIFY_URL, params=data)
+            r = requests.post(self.VERIFY_URL, data=data)
             return r.json()["success"] if r.status_code == 200 else False
         return True
